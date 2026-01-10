ee03b11faa78cf91d8f28d77ba8640e42d89a861
laatu08 <partha2002borah@gmail.com>
2026-01-10 22:12:21 +0530

    server updated

--- internal/raft/server.go
+++ internal/raft/server.go
@@ -8,11 +8,9 @@ import (
 // StartServer starts the HTTP server for this node
 func (n *Node) StartServer(address string) {
 	http.HandleFunc("/request-vote", n.handleRequestVote)
+	http.HandleFunc("/append-entries", n.handleAppendEntries)
 
 	log.Printf("[%s] Listening on %s", n.ID, address)
-
-	// Blocking call — server runs forever
-	if err := http.ListenAndServe(address, nil); err != nil {
-		log.Fatal(err)
-	}
+	http.ListenAndServe(address, nil)
 }
+
