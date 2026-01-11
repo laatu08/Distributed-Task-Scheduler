19978dfbdb759c2dfc903fda84ac13071f02976e
laatu08 <partha2002borah@gmail.com>
2026-01-12 00:01:02 +0530

    tested

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"time"
+
 	"distributed-task-scheduler/internal/scheduler"
+	"distributed-task-scheduler/internal/types"
+	"distributed-task-scheduler/internal/worker"
 	"distributed-task-scheduler/pkg/logger"
 )
 
 func main() {
-	logger.Info("Starting scheduler node")
+	logger.Info("Starting single-node scheduler")
 
 	s := scheduler.NewScheduler()
 
-	_ = s // temporary
-	select {}
+	// Add tasks
+	for i := 1; i <= 10; i++ {
+		s.AddTask(&types.Task{
+			ID:      "task-" + string(rune('A'+i)),
+			Payload: "demo-task",
+		})
+	}
+
+	// Start workers
+	w1 := worker.NewWorker("worker-1", s)
+	w2 := worker.NewWorker("worker-2", s)
+
+	go w1.Start()
+	go w2.Start()
+
+	for {
+		time.Sleep(10 * time.Second)
+	}
 }
